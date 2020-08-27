fe529cfb1f55afb25186623aab3d6919abcf41c8
Martin Grabina <mgrabina@itba.edu.ar>
2020-08-27 20:43:37 -0300

    Base project and rotation included

---
+++ CS2D/Assets/Scripts/Tests/CubeEntity.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeEntity
+{
+    private GameObject gameObject;
+    
+    public CubeEntity(GameObject go){
+        this.gameObject = go;
+    }
+
+    public void Serialize(BitBuffer buffer) {
+        Vector3 position = gameObject.transform.position;
+        Quaternion rotation = gameObject.transform.rotation;
+        buffer.PutFloat(position.x);
+        buffer.PutFloat(position.y);
+        buffer.PutFloat(position.z);
+        buffer.PutFloat(rotation.w);
+        buffer.PutFloat(rotation.x);
+        buffer.PutFloat(rotation.y);
+        buffer.PutFloat(rotation.z);
+    }
+
+    public void Deserialize(BitBuffer buffer) {
+        Vector3 position = new Vector3();
+        Quaternion rotation = new Quaternion();
+        position.x = buffer.GetFloat();
+        position.y = buffer.GetFloat();
+        position.z = buffer.GetFloat();
+        rotation.w = buffer.GetFloat();
+        rotation.x = buffer.GetFloat();
+        rotation.y = buffer.GetFloat();
+        rotation.z = buffer.GetFloat();
+        gameObject.transform.position = position;
+        gameObject.transform.rotation = rotation;
+    }
+}
---
+++ CS2D/Assets/Scripts/Tests/SimulationTest.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class SimulationTest : MonoBehaviour
+{
+
+    private Channel channel;
+    private bool online = true;
+
+    [SerializeField] private GameObject serverGameObject;
+    [SerializeField] private GameObject clientGameObject;
+    
+    private CubeEntity serverCubeEntity;
+    private CubeEntity clientCubeEntity;
+    
+    // Start is called before the first frame update
+    void Start() {
+        channel = new Channel(9000);
+        serverCubeEntity = new CubeEntity(serverGameObject);
+        clientCubeEntity = new CubeEntity(clientGameObject);
+    }
+
+    private void OnDestroy() {
+        channel.Disconnect();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //apply input
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            serverGameObject.GetComponent<Rigidbody>().AddForceAtPosition(Vector3.up * 5, Vector3.zero, ForceMode.Impulse);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.D)) {
+            online = !online;
+        }
+
+        if (online)
+        {
+            UpdateServer();
+        }
+        
+        UpdateClient();
+    }
+
+    private void UpdateServer()
+    {
+        //serialize
+        var packet = Packet.Obtain();
+        serverCubeEntity.Serialize(packet.buffer);
+        packet.buffer.Flush();
+
+        string serverIP = "127.0.0.1";
+        int port = 9000;
+        var remoteEp = new IPEndPoint(IPAddress.Parse(serverIP), port);
+        channel.Send(packet, remoteEp);
+
+        packet.Free();   
+    }
+    
+    private void UpdateClient() {
+        var packet = channel.GetPacket();
+
+        if (packet == null) {
+            // Interpolation
+            return;
+        }
+
+        var buffer = packet.buffer;
+
+        //deserialize
+        clientCubeEntity.Deserialize(buffer);
+    }
+}

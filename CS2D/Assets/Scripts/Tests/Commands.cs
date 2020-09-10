39dad066f57ce2cbbf6748a826ad17eb3b38e755
Martin Grabina <mgrabina@itba.edu.ar>
2020-09-10 19:42:23 -0300

    fixes

---
+++ CS2D/Assets/Scripts/Tests/Commands.cs
@@ -0,0 +1,50 @@
+namespace Tests
+{
+    public class Commands
+    {
+        public int number;
+        public bool up;
+        public bool down;
+        public bool space;
+        public float timestamp;
+
+        public Commands(int number, bool up, bool down, bool space)
+        {
+            this.number = number;
+            this.up = up;
+            this.down = down;
+            this.space = space;
+        }
+    
+        public Commands(int number, bool up, bool down, bool space, float timestamp)
+        {
+            this.number = number;
+            this.up = up;
+            this.down = down;
+            this.space = space;
+            this.timestamp = timestamp;
+        }
+
+        public Commands()
+        {
+        }
+    
+
+        public void Serialize(BitBuffer buffer)
+        {
+            buffer.PutInt(number);
+            buffer.PutBit(up);
+            buffer.PutBit(down);
+            buffer.PutBit(space);
+        }
+    
+        public void Deserialize(BitBuffer buffer)
+        {
+        
+            number = buffer.GetInt();
+            up = buffer.GetBit();
+            down = buffer.GetBit();
+            space = buffer.GetBit();
+        }
+    }
+}
--- CS2D/Assets/Scripts/Tests/CubeEntity.cs
+++ CS2D/Assets/Scripts/Tests/CubeEntity.cs
@@ -5,34 +5,61 @@ using UnityEngine;
 public class CubeEntity
 {
     private GameObject gameObject;
-    
+    Vector3 aux_position = new Vector3();
+    Quaternion aux_rotation = new Quaternion();
+
     public CubeEntity(GameObject go){
         this.gameObject = go;
     }
 
     public void Serialize(BitBuffer buffer) {
-        Vector3 position = gameObject.transform.position;
-        Quaternion rotation = gameObject.transform.rotation;
-        buffer.PutFloat(position.x);
-        buffer.PutFloat(position.y);
-        buffer.PutFloat(position.z);
-        buffer.PutFloat(rotation.w);
-        buffer.PutFloat(rotation.x);
-        buffer.PutFloat(rotation.y);
-        buffer.PutFloat(rotation.z);
+        aux_position = gameObject.transform.position;
+        aux_rotation = gameObject.transform.rotation;
+        buffer.PutFloat(aux_position.x);
+        buffer.PutFloat(aux_position.y);
+        buffer.PutFloat(aux_position.z);
+        buffer.PutFloat(aux_rotation.w);
+        buffer.PutFloat(aux_rotation.x);
+        buffer.PutFloat(aux_rotation.y);
+        buffer.PutFloat(aux_rotation.z);
+        
     }
 
     public void Deserialize(BitBuffer buffer) {
-        Vector3 position = new Vector3();
-        Quaternion rotation = new Quaternion();
-        position.x = buffer.GetFloat();
-        position.y = buffer.GetFloat();
-        position.z = buffer.GetFloat();
-        rotation.w = buffer.GetFloat();
-        rotation.x = buffer.GetFloat();
-        rotation.y = buffer.GetFloat();
-        rotation.z = buffer.GetFloat();
-        gameObject.transform.position = position;
-        gameObject.transform.rotation = rotation;
+        aux_position = new Vector3();
+        aux_rotation = new Quaternion();
+        aux_position.x = buffer.GetFloat();
+        aux_position.y = buffer.GetFloat();
+        aux_position.z = buffer.GetFloat();
+        aux_rotation.w = buffer.GetFloat();
+        aux_rotation.x = buffer.GetFloat();
+        aux_rotation.y = buffer.GetFloat();
+        aux_rotation.z = buffer.GetFloat();
+        gameObject.transform.position = aux_position;
+        gameObject.transform.rotation = aux_rotation;
+    }
+
+    public static CubeEntity createInterpolationEntity(CubeEntity previousEntity, CubeEntity nextEntity, float time)
+    {
+        var entity = new CubeEntity(previousEntity.gameObject);
+        entity.gameObject.transform.position = entity.gameObject.transform.position + Vector3.Lerp(
+                                                   previousEntity.gameObject.transform.position, 
+                                                   nextEntity.gameObject.transform.position, time);
+        var rotation1 = previousEntity.gameObject.transform.rotation;
+        var deltaRotation = Quaternion.Lerp(rotation1,
+            nextEntity.gameObject.transform.rotation, time);
+        var rotation = new Quaternion();
+        rotation.x = rotation1.x + deltaRotation.x;
+        rotation.w = rotation1.w + deltaRotation.w;
+        rotation.y = rotation1.y + deltaRotation.y;
+        rotation.z = rotation1.z + deltaRotation.z;
+        entity.gameObject.transform.rotation = rotation;
+        return entity;
+    }
+
+    public void applyChanges()
+    {
+        gameObject.transform.position = aux_position;
+        gameObject.transform.rotation = aux_rotation;
     }
 }
